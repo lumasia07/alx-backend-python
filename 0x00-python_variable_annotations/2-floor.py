ddc527bb830e62b25a5cf3e1415c6c43f1724ece
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 14:53:35 +0300

    recursive fix

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Module returns floor of a float"""
-from math import floor
+import math
 
 
 def floor(n: float) -> int:
     """Returns floor of float n"""
-    return floor(n)
+    return math.floor(n)
