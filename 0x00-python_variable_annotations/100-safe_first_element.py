6d41153acb67cce40a90a64fcb9e954c2f0c2a7b
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 22:22:55 +0300

    Removed whitespace

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module returns first element in a list"""
-from typing import Sequence, Any, Union 
+from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
