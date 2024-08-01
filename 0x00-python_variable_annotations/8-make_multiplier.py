deb0f49d6c32183e7a3efebabcda297cf4c162b4
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 21:22:12 +0300

    added a return type

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -8,4 +8,4 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def times(val: float) -> float:
         """Returns Callable type"""
         return val * multiplier
-
+    return times
