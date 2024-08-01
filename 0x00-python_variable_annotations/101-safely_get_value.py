5c452921d947ae0b74c302052397ef1917123f99
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 22:47:20 +0300

    TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module returns key in dict"""
+from typing import Mapping, Any, Union, TypeVar 
+
+
+T = TypeVar('T')
+
+def safely_get_value(dct: Mapping[Any, Any], key: Any, default: Union[T, None]  = None) -> Union[Any, T]:
+    """Returns key else default"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
