6a9b7cec83616262e329042e1b9d54fb71453962
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 23:06:47 +0300

    major fix

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """Module returns key in dict"""
-from typing import Mapping, Any, Union, TypeVar
+from typing import Mapping, Any, Union, TypeVar, Optional
 
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any, default:
-                     Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """Returns key else default"""
     if key in dct:
         return dct[key]
