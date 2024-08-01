57f78bcf12fc0ab5fc29fa8ce1f09455d2451a94
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 13:41:53 +0300

    add module

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type annotated module to return a sum of floats"""
+
+
+def add(a: float, b: float) -> float:
+    """Returns sum of a and b as a float"""
+    return a + b
