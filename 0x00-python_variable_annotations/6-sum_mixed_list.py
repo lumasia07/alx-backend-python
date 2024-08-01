efd7ecb831427313e059877d3d8921b46e669067
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 16:00:10 +0300

    sum of mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module to sum list of mixed items"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """Returns sum of mxd list"""
+    return sum(mxd_lst)
+
