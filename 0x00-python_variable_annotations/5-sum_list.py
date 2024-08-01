cfa90d2222d341b79b0c044dcac2ad74ddece64e
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 15:53:16 +0300

    sum of list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module to sum list list items"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Returns sum of input list"""
+    return sum(input_list)
