3319d7fbcc16945feeb37809db71016a3d8a290e
lumasia stancelaus <lumasiastancelaus@gmail.com>
2024-08-01 21:01:24 +0300

    Callable annotation

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module that returns a multiplier function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier"""
+    def times(val: float) -> float:
+        """Returns Callable type"""
+        return val * multiplier
+
