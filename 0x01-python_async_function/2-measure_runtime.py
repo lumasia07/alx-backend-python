02ba122e31e3883b57efe22c4fef112da5dc0d55
Lumasia <lumasiastancelaus@gmail.com>
2024-08-05 20:19:39 +0300

    measure runtime add endline

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,6 +4,7 @@ import asyncio
 import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """Return elapsed time"""
     start = time.perf_counter()
