19ed511b7afecbc2ea02c6057a1622f609d6e582
Lumasia <lumasiastancelaus@gmail.com>
2024-08-26 11:10:02 +0300

    Test Exceptions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""""Test Nested Mapping"""
+"""Test Nested Mapping"""
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -7,13 +7,25 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """Defines a class test for nested map"""
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Test access_nested_map returns expected output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map raises KeyError for invalid paths."""
+        with self.assertRaises(KeyError) as cm:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), f"'{path[-1]}'")
 
 
 if __name__ == "__main__":
